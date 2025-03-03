0967c22808b1c79eefb3bab64b92fa003821f31c
GitHack27 <arceusxy2003@gmail.com>
2025-03-03 10:19:20 +0530

    Home Page Setup with Header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { OneLinerBannerComponent } from "../Shared/one-liner-banner/one-liner-banner.component";
+import { HeaderComponent } from "../Shared/header/header.component";
+import { CommonModule } from '@angular/common';
+import { CourseCardComponent } from "../Shared/course-card/course-card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [OneLinerBannerComponent, HeaderComponent, CommonModule, CourseCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
+  counters = [
+    { value: 100, label: 'Courses', currentValue: 0 },
+    { value: 12, label: 'Countries', currentValue: 0 },
+    { value: 500, label: 'Students', currentValue: 0 },
+    { value: 10, label: 'Instructors', currentValue: 0 }
+  ];
 
+  @ViewChild('testimonialBanner') banner!: ElementRef;
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.startCounting();
+          observer.disconnect();
+        }
+      });
+    });
+
+    observer.observe(this.banner.nativeElement);
+  }
+
+  startCounting() {
+    this.counters.forEach(counter => {
+      let step = Math.ceil(counter.value / 50);
+      let interval = setInterval(() => {
+        counter.currentValue += step;
+        if (counter.currentValue >= counter.value) {
+          counter.currentValue = counter.value;
+          clearInterval(interval);
+        }
+      }, 30);
+    });
+  }
 }
