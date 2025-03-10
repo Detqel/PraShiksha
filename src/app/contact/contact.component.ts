75761c751167540bf8b8d2a04359d8af96478ec7
GitHack27 <arceusxy2003@gmail.com>
2025-03-10 22:05:15 +0530

    Completed About, Contact, Pricing

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../Shared/header/header.component";
+import { FooterComponent } from "../Shared/footer/footer.component";
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../Shared/header/header.component";
+import { FooterComponent } from "../Shared/footer/footer.component";
 
 @Component({
   selector: 'app-pricing',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.css'
 })
